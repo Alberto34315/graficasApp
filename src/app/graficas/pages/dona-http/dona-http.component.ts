914d4e5496a637d047fdda636f728856c48e36e9
Alberto Ruiz <ruizserranoalberto5@gmail.com>
2022-04-06 18:24:17 +0200

    first commit

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js';
+import { GraficasService } from '../../services/graficas.service';
 
 @Component({
   selector: 'app-dona-http',
@@ -7,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class DonaHttpComponent implements OnInit {
-
-  constructor() { }
+  public doughnutChartLabels: string[] = [];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    // labels: this.doughnutChartLabels,
+    datasets: [
+      // { data: [350, 450, 100], backgroundColor: ["#E6992E", "#99FC26", "#5C8FF5"], hoverBackgroundColor: 'red' },
+      //  { data: [ 50, 150, 120 ] },
+      // { data: [ 250, 130, 70 ] }
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+  constructor(private graficasService: GraficasService) { }
 
   ngOnInit(): void {
+    // this.graficasService.getUsuarioRedesSociale().subscribe(data => {
+    //   const labels = Object.keys(data)
+    //   const values = Object.values(data)
+    //   this.doughnutChartLabels = labels;
+    //   this.doughnutChartData = {
+    //     labels: this.doughnutChartLabels,
+    //     datasets: [{
+    //       data: values
+    //     }]
+    //   }
+    // })
+
+    this.graficasService.getusuariosRedesSocialesDonaData()
+      .subscribe(({ labels, values }) => {
+        this.doughnutChartData = {
+          labels: labels,
+          datasets: [{
+            data: values
+          }]
+        }
+      })
+
+
   }
 
 }
--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartType, Color } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class DonaComponent implements OnInit {
-
+  public doughnutChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [350, 450, 100], backgroundColor: ["#E6992E", "#99FC26", "#5C8FF5"], hoverBackgroundColor: 'red' },
+      //  { data: [ 50, 150, 120 ] },
+      // { data: [ 250, 130, 70 ] }
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
   constructor() { }
 
   ngOnInit(): void {
